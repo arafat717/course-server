0d9cf00b45946bf10de12a1237f01fe8f4c52cb0
arafat717 <arafatjibon33@gmail.com>
2025-05-25 20:16:02 +0600

    added meta to queryBuilder

--- src/modules/Course/Course.service.ts
+++ src/modules/Course/Course.service.ts
@@ -35,9 +35,9 @@ const getAllCourseFromDb = async (query: Record<string, unknown>) => {
     .sort()
     .paginate()
     .fields();
-
+  const meta = await courseQuery.countTotal();
   const result = await courseQuery.modelQuery;
-  return result;
+  return { meta, result };
 };
 
 const getSingleCourseFromDb = async (id: string) => {
